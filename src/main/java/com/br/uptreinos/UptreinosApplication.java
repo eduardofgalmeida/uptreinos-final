6ff9b6fcf6d44a328fb495ef43f64687d9dad021
Eduardo Almeida <eduardo.fgalmeida@gmail.com>
2022-02-08 20:51:20 -0300

    Primeiro commit

---
+++ src/main/java/com/br/uptreinos/UptreinosApplication.java
@@ -0,0 +1,13 @@
+package com.br.uptreinos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptreinosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UptreinosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/uptreinos/UptreinosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.uptreinos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptreinosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
